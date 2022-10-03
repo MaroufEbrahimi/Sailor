1c20ceb2c25d4d6eda9decb8fa5c72aade46cfb2
marouf <maroufibrahimi7264@gmail.com>
2022-10-02 19:45:02 -0700

    slider for showcase

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+// Header link activate
 const link_active = document.querySelectorAll('.link_active');
 function activeHeaderLink() {
    link_active.forEach(ele => {
@@ -9,3 +10,24 @@ function activeHeaderLink() {
 link_active.forEach(ele => {
    ele.addEventListener('click', activeHeaderLink);
 });
+
+
+// showcase slider
+const showcase_slider = document.querySelectorAll('.showcase_content');
+let current_slide = 0;
+showcase_slider[current_slide].style.opacity = 1;
+
+setInterval(() => {
+   next_slide();
+}, 5000);
+
+function next_slide() {
+   showcase_slider[current_slide].style.zIndex = -2;
+   let temp = showcase_slider;
+   setTimeout(() => {
+      showcase_slider[temp].style.opacity = 0;
+   }, 1000);
+   current_slide = (current_slide + 1) % showcase_slider.length;
+   showcase_slider[current_slide].style.opacity = 1;
+   showcase_slider[current_slide].style.zIndex = -1;
+}
