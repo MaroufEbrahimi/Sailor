a13b8c140d5cc963fdbd2547a8700dfa6658eacc
marouf <maroufibrahimi7264@gmail.com>
2022-10-04 22:18:43 -0700

    active the back-to-top button when scroll the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,15 @@ function next_slide() {
    showcase_slider[current_slide].style.opacity = 1;
    showcase_slider[current_slide].style.zIndex = -1;
 }
+
+// back to top button
+const back_to_top = document.querySelector('.back_to_top');
+window.addEventListener('scroll', function () {
+   if (this.window.scrollY > 150) {
+      back_to_top.style.opacity = 1;
+      back_to_top.style.cursor = 'pointer';
+   } else {
+      back_to_top.style.opacity = 0;
+      back_to_top.style.cursor = 'default';
+   }
+});
